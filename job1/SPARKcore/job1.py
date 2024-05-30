21f4416ad2fd6f67e509acf0f072ca0faa6eb8d2
FabLetizia <fabio.letizia01@gmail.com>
2024-05-30 17:27:28 +0200

    update job1 with sparkcore

--- job1/SPARKcore/job1.py
+++ job1/SPARKcore/job1.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-"""spark aplication"""
 
 from datetime import datetime
 import argparse
@@ -8,8 +7,9 @@ import csv
 from io import StringIO
 
 def parse_line(line):
-    # Usa la libreria csv per leggere la linea correttamente
+    # Extract the fields
     fields = next(csv.reader(StringIO(line), delimiter=';'))
+    # Skip the header (first line)
     if fields[0] == "ticker":
         return None
     ticker = fields[0]
@@ -20,21 +20,27 @@ def parse_line(line):
     high = float(fields[4])
     volume = float(fields[5])
     name = fields[8]
+    # (key, values)
     return ((ticker, year), (date, close, low, high, volume, name))
  
-def sort_and_calculate_stats(values):
+''' This function takes the values ​​associated with a key (ticker, year), sorts them by date,
+then calculates yearly statistics such as percentage change in price, lowest price, highest price,
+and average volume. Returns a tuple with these statistics.'''
+def calculate_stats(values):
     sorted_values = sorted(values, key=lambda x: x[0])
     _, close_prices, low_prices, high_prices, volumes, name = zip(*sorted_values)
     first_close = close_prices[0]
     last_close = close_prices[-1]
-    percentual_variation_rounded = round(((last_close - first_close) / first_close) * 100, 2)
+    # All rounded to 2 decimal places
+    percentual_variation = round(((last_close - first_close) / first_close) * 100, 2)
     max_high = round(max(high_prices),2)
     min_low = round(min(low_prices),2)
     mean_volume = round(sum(volumes) / len(volumes),2)
-    return (name[0], percentual_variation_rounded, min_low, max_high, mean_volume)
+    return (name[0], percentual_variation, min_low, max_high, mean_volume)
 
  
 parser = argparse.ArgumentParser()
+# input and output paths passed from the command line
 parser.add_argument("--input_path", type=str, help="Input dataset path")
 parser.add_argument("--output_path", type=str, help="Output folder path")
  
@@ -46,17 +52,15 @@ spark = SparkSession \
     .appName("Stock Annual Trend") \
     .getOrCreate()
  
- 
 lines = spark.sparkContext.textFile(dataset_filepath).cache()
 data = lines.map(parse_line).filter(lambda x: x is not None).groupByKey()
 
-stats_per_stock_per_year = data.mapValues(sort_and_calculate_stats)
-output = stats_per_stock_per_year.map(lambda x: (x[0], x[1]))
- 
-# Ordina l'output in base al ticker
+stats_stock_year = data.mapValues(calculate_stats)
+output = stats_stock_year.map(lambda x: (x[0], x[1]))
+
 output_sorted = output.sortByKey()
 
-# Riduci il numero di partizioni a 1 prima di salvare l'output
+# Reduce the number of partitions to 1 before saving the output
 output_sorted.coalesce(1).saveAsTextFile(output_filepath)
 
 spark.stop()
